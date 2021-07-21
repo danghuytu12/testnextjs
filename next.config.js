5dbad4f499dd77cc035abc090af581005d2b8e3a
Huy Tú <you@example.com>
2021-07-21 22:52:44 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        "BASE_URL": "http://localhost:3000",
+        "MONGODB_URL": "mongodb+srv://Huytu:Danghuytu1234@cluster0.euiti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        "ACCESS_TOKEN_SECRET": "z6E?G),:&v'w2-nES%]pAKQ-!Ztarp-r;UEj49Nv:Yh@eK'WVC",
+        "REFRESH_TOKEN_SECRET": "vn+F48@s-%{w2<:+-Z$S8WY'%kKm,UShgQaaY&h}`(,8NN(G3m(b?]^85-2%t.X:MQ_R#UUv@U%UgGXs",
+        "PAYPAL_CLIENT_ID": "Ad9s8dWwZ3hS10EOwUD7v23ZreYdvwXj8Nn3ZtnT2ruP48lOaGpuz0O6zJoXfiNfs2p0XKCsXg_zkZ-V",
+        "CLOUD_UPDATE_PRESET": "nextjs_store",
+        "CLOUD_NAME": "banhang123",
+        "CLOUD_API": "https://api.cloudinary.com/v1_1/banhang123/image/upload"
+    }
+}
