8752d11ea6dda8586fa11d5982f12ebf522a8639
danghuytu12 <86486246+danghuytu12@users.noreply.github.com>
2022-02-02 19:27:16 +0700

    remove text

--- pages/signin.js
+++ pages/signin.js
@@ -58,7 +58,6 @@ const Signin = () => {
             <label htmlFor="exampleInputEmail1">Email address</label>
             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
             name="email" value={email} onChange={handleChangeInput} />
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Password</label>
